320132ffd67b2c8e8ab20f0ebdb27a75887926c3
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-06-15 12:39:42 +0530

    booking get

--- Backend/routes/booking.js
+++ Backend/routes/booking.js
@@ -13,13 +13,25 @@ router.post('/',async (req,res)=>{
 })
 
 
-router.get('/', async (req,res)=>{
-    try{
-        const {user,journeyDate} = req.query;
-        const busDetails = await Bus.find({user,journeyDate});
-        res.send( busDetails)
-    }catch(error){
-res.status(500).send(error)
+// router.get('/', async (req,res)=>{
+//     try{
+//         const {user,journeyDate} = req.query;
+//         const busDetails = await Bus.find({user,journeyDate});
+//         res.send( busDetails)
+//     }catch(error){
+// res.status(500).send(error)
+//     }
+// })
+
+router.get('/',async (req,res)=>{
+    {
+        try{
+            const booking = await Booking.find();
+           
+            res.send(booking)
+        }catch(error){
+            res.status(500).send(error)
+        }
     }
 })
 
